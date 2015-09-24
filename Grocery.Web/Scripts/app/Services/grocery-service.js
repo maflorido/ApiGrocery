04c4e08cca1556c8a5c1ca438b7a216f3c849095
y8p9 <marcello.amaral1@hotmail.com>
2015-09-24 13:37:46 -0300

    código funcional

---
+++ Grocery.Web/Scripts/app/Services/grocery-service.js
@@ -0,0 +1,10 @@
+﻿app.factory('GroceryService', ['$http', function ($http) {
+    var urlBase = '/api/GroceryShop';
+    var GroceryService = {};
+
+    GroceryService.GetProducts = function () {
+        return $http.get(urlBase);
+    };
+
+    return GroceryService;
+}]);
