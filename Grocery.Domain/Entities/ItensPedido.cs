c3d0aee91215adba0b9bc98b0e15df3091ffc243
y8p9 <marcello.amaral1@hotmail.com>
2015-11-16 09:42:44 -0200

    implementação inicial do pedido.

---
+++ Grocery.Domain/Entities/ItensPedido.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.Domain.Entities
+{
+    public class ItensPedido
+    {
+        public Produto Produto { get; set; }
+
+        public long Quantidade { get; set; }
+
+        public double ValorItem
+        {
+            get { return ValorItem; }
+            set { value = this.Produto.Valor; }
+        }
+
+        public double Total
+        {
+            get{ return Total; }
+            set { value = this.ValorItem * this.Quantidade; }
+        }
+    }
+}
---
+++ Grocery.Domain/Entities/Pedido.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.Domain.Entities
+{
+    public class Pedido
+    {
+        public Pedido()
+        {
+            this.ItensPedido = new List<ItensPedido>();
+        }
+
+        public long Id { get; private set; }
+
+        public DateTime DataPedido { get; set; }
+
+        public string CPF { get; set; }
+
+        public string CEP { get; set; }
+
+        public string Endereco { get; set; }
+
+        public double Total { get; set; }
+
+        public virtual IList<ItensPedido> ItensPedido { get; set; }
+    }
+}
